804f1aa3ccc8ca6ba867c069cdf69048f9c13fbc
natnael <natnaelargaw@gmail.com>
2016-05-26 18:33:47 +0300

    builds deleted

--- build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/natnael/opencog-audiovisual/src
-# Build directory: /home/natnael/opencog-audiovisual/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-SUBDIRS(gtest)
-SUBDIRS(audiosysneeds)
-SUBDIRS(visualsysneeds)
